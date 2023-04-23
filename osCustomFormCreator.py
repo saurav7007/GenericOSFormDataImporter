a6622d9fdce02cf5848efdfa718eba61f628cd58
Saurav Raj Singh <67055370+saurav7007@users.noreply.github.com>
2023-04-23 11:53:38 -0700

    Add files via upload

---
+++ osCustomFormCreator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 07:03:29 2023
+
+@author: krishagni
+"""
+
+import configparser, pandas as pd
+
+#### Call config object #########
+config_obj = configparser.ConfigParser()
+
+#### Read Config file ###########
+config_obj.read("configfile.ini")
+
+#### Read path from input file from Config file ##############
+path_of_input_file = config_obj["input_file_path"]["path_of_input_file"]
+path_of_output_folder = config_obj["output_folder"]["path_of_output_folder"]
+module = config_obj["module"]["module_type"]
+
+#### Read input file using pandas module ################
+rawData = pd.read_csv(path_of_input_file, sep = ',' , low_memory = False)
+
+#### Group the data by Form_Name #######
+groups = rawData.groupby('Form_Name')
+
+for name, group in groups:    
+    #### Pivot the DataFrame to reshape #######
+    df_pivot = group.pivot_table(index=['CP_Title', module], columns='Field_Name', values='Field_Value', aggfunc='first')  
+    #### Takes the first value if there are multiple values for a given combination of index and column.
+    
+    #### Reset the index to turn the MultiIndex into columns ####
+    df_pivot = df_pivot.reset_index()
+    
+    #### Output csv file ######
+    df_pivot.to_csv(f'{path_of_output_folder}/{name}.csv')
